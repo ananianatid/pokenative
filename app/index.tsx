a1af30c9c64b22a0d4cd7b6de20d63f7e53e2e20
ananianatid <ananianatid@gmail.com>
2026-05-05 14:52:15 +0000

    Ecran de listing : Add FlatList to display Pokemon in a grid layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View  } from "react-native";
+import {FlatList, StyleSheet, Text, View  } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedText } from "@/components/ThemedText";
@@ -7,17 +7,31 @@ import { Card } from "@/components/Card";
 import { Image } from "react-native";
 export default function Index() {
   const colors = useThemeColors()
+  const pokemons = Array.from({ length: 35 }, (_, k) => ({
+    name: `Pokemon ${k + 1}`,
+     id: k + 1
+  }));
   return (
     <SafeAreaView style={[Styles.container,{ backgroundColor: colors.tint }]} >
       <View style={Styles.header}>
         <Image source={require('@/assets/images/pokeball.png')} width={24} height={24} />
         <ThemedText variant="headline" color="grayLight">Pokemon</ThemedText>
       </View>
-      <Card style={Styles.body}></Card>
+      <Card style={Styles.body}>
+        <FlatList 
+        data={pokemons} 
+        numColumns={3}
+        contentContainerStyle={[Styles.gridGap, Styles.list]}
+        columnWrapperStyle={Styles.gridGap}
+        renderItem={({item})=>
+          <Card style={{flex:1/3,height:200}}>
+            <Text>{item.name}</Text>
+          </Card>
+        } keyExtractor={(item) => item.id.toString()} ></FlatList>
+      </Card>
     </SafeAreaView>
   );
 }
-const pokeid = 3;
 const Styles = StyleSheet.create({
   container: {
     flex:1 ,
@@ -31,5 +45,11 @@ const Styles = StyleSheet.create({
   },
   body: {
     flex: 1,
+  },
+  gridGap:{
+    gap: 8
+  },
+  list: {
+    padding: 12,
   }
 })  
