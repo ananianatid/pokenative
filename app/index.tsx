a768afd52ab9ce1ce0b0cbe8663d0fc499577e4b
ananianatid <ananianatid@gmail.com>
2026-05-05 14:38:27 +0000

    Refactor Index component layout and add Pokeball image

--- app/index.tsx
+++ app/index.tsx
@@ -4,21 +4,32 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedText } from "@/components/ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { Card } from "@/components/Card";
+import { Image } from "react-native";
 export default function Index() {
   const colors = useThemeColors()
   return (
     <SafeAreaView style={[Styles.container,{ backgroundColor: colors.tint }]} >
-      <Card>
-        <ThemedText variant="headline" color="grayDark">Pokemon</ThemedText>
-      </Card>
-      {/* <Link href="/about">A propos</Link> */}
-      {/* <Link href={{pathname:'/pokemon/[id]',params:{id: pokeid}}}>Pokemon {pokeid}</Link> */}
+      <View style={Styles.header}>
+        <Image source={require('@/assets/images/pokeball.png')} width={24} height={24} />
+        <ThemedText variant="headline" color="grayLight">Pokemon</ThemedText>
+      </View>
+      <Card style={Styles.body}></Card>
     </SafeAreaView>
   );
 }
 const pokeid = 3;
 const Styles = StyleSheet.create({
   container: {
-    flex:1 
+    flex:1 ,
+    padding: 4
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+    padding: 12
+  },
+  body: {
+    flex: 1,
   }
 })  
